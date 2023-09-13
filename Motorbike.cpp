409c09ea4af7847354f7f93b3f6361f4e2935064
s3986878 <143153523+s3986878@users.noreply.github.com>
2023-09-13 13:24:16 +0700

    Update Motorbike.cpp

---
+++ Motorbike.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+class Motorbike{
+    private:
+    int bikeID;
+    string brand;
+    Member userID;
+    string city;
+    int requiredScore;
+   bool isRented = 0;
+   vector <Motorbike *> motorlist;
+   public:
+   Motorbike(int bikeID = 0, Member userID = 0, string city = "", int requiredScore = 0, bool isRented = false, vector <Motorbike*> motorlist = {}) 
+            : bikeID(bikeID), userID(userID), city(city), requiredScore(requiredScore), isRented(isRented), motorlist(motorlist){} 
+    
+    int findbike(int ID){
+        for(int i = 0; i < motorlist.size(); i++){
+            if(motorlist[i]->bikeID == ID){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    void addbike(){
+        int bikeID;
+        cout << "Please enter license plate: ";
+        cin >> bikeID;
+
+        int findbikeID = findbike(bikeID);
+        if (findbikeID != -1)
+        {
+            cout << "Bike already exist" << endl;
+        }else{
+            cout << "Please enter the brand of bike: ";
+            cin.ignore();
+            getline(cin, brand);
+            cout << "Enter city: ";
+            cin.ignore();
+            cin >> city;
+            cout << "Enter required score: ";
+            cin >> requiredScore;
+
+            //motorlist.push_back(bikeID, brand, city, requiredScore);
+        }
+    }
+
+};
+
