d01758b7b1ed521ef1991ab19c5fe42689191fa1
thaobwi <129365706+thaobwi@users.noreply.github.com>
2023-09-23 14:29:09 +0700

    Update Motorbike.cpp

--- motorbike_rentalapp_upate/Motorbike.cpp
+++ motorbike_rentalapp_upate/Motorbike.cpp
@@ -2,14 +2,17 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <string.h>
 #include <sstream>
+#include <cmath>
 #include "Motorbike.h"
 #include "Member.h"
 #include "functions.h"
 
 using namespace std;
 
-Motorbike::Motorbike(int ownerID, string bikeID, string city, double requiredScore, bool isRented, double motorbike_rating_score, double credit, string brand, string model, string color, string description, string engine_size){
+Motorbike::Motorbike(int ownerID, string bikeID, string city, double requiredScore, bool isRented, double motorbike_rating_score, double credit, string brand, string model, string color, string description, string engine_size)
+{
     this->ownerID = ownerID;
     this->bikeID = bikeID;
     this->city = city;
@@ -24,62 +27,91 @@ Motorbike::Motorbike(int ownerID, string bikeID, string city, double requiredSco
     this->engine_size = engine_size;
 }
 
-Motorbike::Motorbike(){
-
+Motorbike::Motorbike()
+{
 }
 
-void Motorbike::setOwnerID(Member member){
+void Motorbike::setOwnerID(Member member)
+{
     this->ownerID = member.getID();
 }
 
-int Motorbike::getOwnerID(){
+int Motorbike::getOwnerID()
+{
     return this->ownerID;
 }
 
-double Motorbike::getCredit(){
+double Motorbike::getCredit()
+{
     return this->credit;
 }
 
-double Motorbike::getRequiredScore(){
+double Motorbike::getRequiredScore()
+{
     return this->requiredScore;
 }
 
-string Motorbike::getID(){
+string Motorbike::getID()
+{
     return this->bikeID;
 }
 
-bool Motorbike::isRent(){
+bool Motorbike::isRent()
+{
     return this->isRented;
 }
 
 string Motorbike::toString()
 {
-    return to_string(ownerID) + "," + bikeID + "," + city + "," + brand + "," + model + "," + color + "," + engine_size + "," + description + "," + to_string(round(credit)) + "," + to_string(round(requiredScore)) + "," + to_string(round(motorbike_rating_score)) + "," + to_string(isRented);
+    return to_string(ownerID) + "," + bikeID + "," + city + "," + brand + "," + model + "," + color + "," + engine_size + "," + description + "," + to_string(credit) + "," + to_string(round(requiredScore)) + "," + to_string(round(motorbike_rating_score)) + "," + to_string(isRented);
 }
 
-string Motorbike::findbike(string ID){
+string Motorbike::findbike(string ID)
+{
     vector<Motorbike> motorlist = motorbikeList();
-    for(int i = 0; i < motorlist.size(); i++){
-        if(motorlist[i].bikeID == ID){
+    for (int i = 0; i < motorlist.size(); i++)
+    {
+        if (motorlist[i].bikeID == ID)
+        {
             return motorlist[i].bikeID;
         }
     }
     return "";
 }
 
-void Motorbike::addbike(Member member){
+void Motorbike::addbike(Member member)
+{
     vector<Motorbike> motorlist = motorbikeList();
     string bikeID;
+    string a = "Ha Noi";
+    string b = "Sai Gon";
+    int index = 1;
     cout << "Please enter license plate: ";
     cin >> bikeID;
+    cin.ignore(1, '\n');
 
     string findbikeID = findbike(bikeID);
-    if (findbikeID != "") {
+    if (findbikeID != "")
+    {
         cout << "Bike already exist" << endl;
-    } else {
-        cout << "Enter city: ";
-        cin.ignore();
-        getline(cin, city);
+    }
+    else
+    {
+        while (index != 0)
+        {
+            cout << "Enter city: ";
+            getline(cin, city);
+
+            if (city.compare(a) == 0 || city.compare(b) == 0)
+            {
+                break;
+            }
+            else if (city.compare(a) != 0 || city.compare(b) != 0)
+            {
+                cout << "The application is only available to users in Ha Noi and Sai Gon!" << endl;
+            }
+        }
+
         cout << "Please enter the brand of bike: ";
         getline(cin, brand);
         cout << "Please enter model of bike: ";
@@ -90,91 +122,118 @@ void Motorbike::addbike(Member member){
         getline(cin, engine_size);
         cout << "Please enter description: ";
         getline(cin, description);
+
         cout << "Please enter credit of bike: ";
         cin >> credit;
-        cout << "Enter required score: ";
-        cin >> requiredScore;        
 
-        Motorbike newMotor(member.getID(), bikeID, city, requiredScore, false, 0, credit, brand, model, color, description, engine_size);
-        newMotor.setOwnerID(member);
-
-        savebike(newMotor, "motorbikes.txt");
-
-        motorlist.push_back(newMotor);
-        cout << "Added successfully" << endl;
-    }            
-}    
-
-void Motorbike::removebike(Motorbike motorbike){
-        ifstream motor("motorbikes.txt");
-
-        if (motor.is_open()){
-            int temp;
-            string myString, line;
-
-            while(getline(motor, line)){
-                stringstream ss(line);
-                getline(ss, myString, ',');
-                ownerID = stoi(myString);
-                getline(ss, bikeID, ',');
-                getline(ss, city, ',');
-                getline(ss, brand, ',');
-                getline(ss, model, ',');
-                getline(ss, color, ',');
-                getline(ss, engine_size, ',');
-                getline(ss, description, ',');
-                getline(ss, myString, ',');
-                credit = stoi(myString);
-                getline(ss, myString, ',');
-                requiredScore = stoi(myString);
-                getline(ss, myString, ',');
-                temp = stoi(myString);
-
-                if (temp == 0){
-                    isRented = false;
-                } else {
-                    isRented = true;
-                }
-
-                Motorbike newMotor(ownerID, bikeID, city, requiredScore, isRented, motorbike_rating_score, credit, brand, model, color, description, engine_size);
-
-                if (newMotor.bikeID != motorbike.bikeID){
-                    savebike(newMotor, "new.txt");
-                } else {
-                    
-                }
+        while (index != 0)
+        {
+            cout << "Enter required score: ";
+            cin >> requiredScore;
+            if (requiredScore <= 10 && requiredScore >= 0)
+            {
+                break;
             }
-            motor.close();
+            else
+            {
+                cout << "The required score must be from 0 to 10!" << endl;
+            }
         }
+    }
 
-        cout<<"Motorbike is removed!\n";
-        remove("motorbikes.txt");
-        rename("new.txt", "motorbikes.txt");
-}      
+    Motorbike newMotor(member.getID(), bikeID, city, requiredScore, false, 0, credit, brand, model, color, description, engine_size);
+    newMotor.setOwnerID(member);
 
-void Motorbike::showinfo(){
-        cout<<"Owner's ID: "<< this->ownerID << "\n";
-        cout<<"Bike's ID: "<< this->bikeID << "\n";
-        cout<<"Brand: "<< this->brand << "\n";
-        cout<<"Model: "<< this->model << "\n";
-        cout<<"Color: "<< this->color << "\n";
-        cout<<"Engine size: "<< this->engine_size << "\n";
-        cout<<"Description: "<< this->description << "\n";
-        cout<<"Credit points: "<< this->credit << "\n";
-        cout<<"Required rating score: "<< this->requiredScore << "\n";
-        cout<<"Rating score: "<< this->motorbike_rating_score << "\n";
-        cout<<"Rented: "<< this->isRented << "\n";
-        cout<<"_____________\n";
-}    
+    savebike(newMotor, "motorbikes.txt");
 
-void Motorbike::savebike(Motorbike newMotor, string filename){
+    motorlist.push_back(newMotor);
+    cout << "Added successfully" << endl;
+}
+
+void Motorbike::removebike(Motorbike motorbike)
+{
+    ifstream motor("motorbikes.txt");
+
+    if (motor.is_open())
+    {
+        int temp;
+        string myString, line;
+
+        while (getline(motor, line))
+        {
+            stringstream ss(line);
+            getline(ss, myString, ',');
+            ownerID = stoi(myString);
+            getline(ss, bikeID, ',');
+            getline(ss, city, ',');
+            getline(ss, brand, ',');
+            getline(ss, model, ',');
+            getline(ss, color, ',');
+            getline(ss, engine_size, ',');
+            getline(ss, description, ',');
+            getline(ss, myString, ',');
+            credit = stoi(myString);
+            getline(ss, myString, ',');
+            requiredScore = stoi(myString);
+            getline(ss, myString, ',');
+            temp = stoi(myString);
+
+            if (temp == 0)
+            {
+                isRented = false;
+            }
+            else
+            {
+                isRented = true;
+            }
+
+            Motorbike newMotor(ownerID, bikeID, city, requiredScore, isRented, motorbike_rating_score, credit, brand, model, color, description, engine_size);
+
+            if (newMotor.bikeID != motorbike.bikeID)
+            {
+                savebike(newMotor, "new.txt");
+            }
+            else
+            {
+            }
+        }
+        motor.close();
+    }
+
+    cout << "Motorbike is removed!\n";
+    remove("motorbikes.txt");
+    rename("new.txt", "motorbikes.txt");
+}
+
+void Motorbike::showinfo()
+{
+    cout << "Owner's ID: " << this->ownerID << "\n";
+    cout << "Bike's ID: " << this->bikeID << "\n";
+    cout << "Brand: " << this->brand << "\n";
+    cout << "Model: " << this->model << "\n";
+    cout << "Color: " << this->color << "\n";
+    cout << "Engine size: " << this->engine_size << "\n";
+    cout << "Description: " << this->description << "\n";
+    cout << "Credit points: " << this->credit << "\n";
+    cout << "Required rating score: " << this->requiredScore << "\n";
+    cout << "Rating score: " << this->motorbike_rating_score << "\n";
+    cout << "Rented: " << this->isRented << "\n";
+    cout << "_____________\n";
+}
+
+void Motorbike::savebike(Motorbike newMotor, string filename)
+{
     ifstream file(filename);
-    if (file.is_open()){
+    if (file.is_open())
+    {
         ofstream file(filename, ios::app);
-        file << "\n" << newMotor.toString();
-    } else {
+        file << "\n"
+             << newMotor.toString();
+    }
+    else
+    {
         ofstream file(filename, ios::app);
         file << newMotor.toString();
     }
     file.close();
-}        
+}
